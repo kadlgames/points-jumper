b34cd5f716136a5fa6ef1e09f448e99c54bfd4e5
dll31 <danila.litvinov31@yandex.ru>
2021-11-01 17:55:01 +0300

    Добавлен текст для подсчета очков

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int _nowScore = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int getCurrentCircleScore()
+    {
+        int score = 0;
+        
+        return score;
+    }
+}
