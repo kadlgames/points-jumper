69a33e5e4dc935c13ef5ef9a220c6224219d8f09
QuandisS <anton.kravtsov.31@gmail.com>
2022-04-08 12:40:25 +0300

    preparing for new gameplay reset system

---
+++ Assets/Scripts/Managers/GameplyInitializer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class creates default gameplay env.
+/// Using when inst scene for first time and 
+/// then for reset after GO
+/// </summary>
+public class GameplyInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
